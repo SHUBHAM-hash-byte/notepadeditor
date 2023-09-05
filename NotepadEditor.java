ca62e447bb1a963fd476fc9606764400d2ca63e0
SHUBHAM-hash-byte <shubhamjaysingpure11@gmail.com>
2023-09-05 18:52:06 +0530

    Initial commit

---
+++ NotepadEditor.java
@@ -0,0 +1,15 @@
+package notepadeditor;
+
+import java.io.FileInputStream;
+
+/**
+ *
+ * @author shubham jaysingpure
+ */
+public class NotepadEditor
+{
+    public static void main(String[] args) throws Exception
+    {
+        new Notepad();
+    }
+}
